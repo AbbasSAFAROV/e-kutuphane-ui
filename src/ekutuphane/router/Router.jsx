b23a5f59080c25baa8748d1c7fe18cbc69ada5c4
AbbasSAFAROV <abbasabdullaziz@gmail.com>
2024-07-31 16:14:24 +0300

    layout taşındı

--- src/ekutuphane/router/Router.jsx
+++ src/ekutuphane/router/Router.jsx
@@ -1,7 +1,7 @@
 import { lazy, Suspense } from 'react';
 import { Outlet, Navigate, useRoutes } from 'react-router-dom';
 
-import DashboardLayout from 'src/layouts/dashboard';
+import Layout from 'src/ekutuphane/layout/Layout';
 
 export const Dashboard = lazy(() => import('src/ekutuphane/Dashboard'));
 export const BlogPage = lazy(() => import('src/pages/blog'));
@@ -16,11 +16,11 @@ export default function Router() {
   const routes = useRoutes([
     {
       element: (
-        <DashboardLayout>
+        <Layout>
           <Suspense>
             <Outlet />
           </Suspense>
-        </DashboardLayout>
+        </Layout>
       ),
       children: [
         { element: <Dashboard />, index: true },
